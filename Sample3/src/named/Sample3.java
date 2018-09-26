e1e722a17599d77279dc2d5b0f549bfc7449185a
Choi Hau Yin <iamsam06@yahoo.com.hk>
2018-09-26 14:53:33 +0800

    Project init

---
+++ Sample3/src/named/Sample3.java
@@ -0,0 +1,9 @@
+package named;
+
+public class Sample3 {
+	public static void main(String[] args){
+		System.out.println('A');
+		System.out.println("Åwªï¨Ï¥ÎJava!");
+		System.out.println(123);
+	}
+}
